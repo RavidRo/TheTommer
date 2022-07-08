e29a95d6a4cdd831d967240777a589d83995b368
ravidro <ravidro@post.bgu.ac.il>
2022-07-08 18:30:09 +0300

    Possesing Objects

---
+++ Assets/Scripts/ShapeShifter.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShapeShifter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        // if (Input.GetKeyDown(KeyCode.Space)){
+        //     // Check if standing on an object
+        //     if (Physics2D.OverlapCircle(transform.position, 0.2f, 1 << LayerMask.NameToLayer("Ground"))){
+        //         // If standing on an object, change to the other object
+        //         if (gameObject.name == "Player"){
+        //             gameObject.name = "Player2";
+        //         }
+        //         else{
+        //             gameObject.name = "Player";
+        //         }
+        //     }
+        // }
+    }
+}
