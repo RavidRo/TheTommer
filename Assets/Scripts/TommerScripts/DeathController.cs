a8f31574fb0653a28731aa6a235111d5b0d0bae1
Ravid Rom <ravidro@post.bgu.ac.il>
2022-07-28 19:17:02 +0300

    You Exposured

--- Assets/Scripts/TommerScripts/DeathController.cs
+++ Assets/Scripts/TommerScripts/DeathController.cs
@@ -2,19 +2,30 @@ using interfaces;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 public class DeathController : MonoBehaviour, IDeathSubscriber
 {
     private Animator animator;
+
     [SerializeField] GameObject tommer;
     [SerializeField] PlayerController player;
     [SerializeField] float deathAnimationTime = 1.5f;
+    [SerializeField] float finalDeadAnimationTime = 7f;
     [SerializeField] float spawnAnimationTime = 1f;
-    [SerializeField] AudioSource audioSource;
+    [SerializeField] AudioSource audioSourceDeath;
+    [SerializeField] AudioSource audioSourceFinalDeath;
+    [SerializeField] Animator UIDeathAnimator;
+
     private float animationTimerCount = 0;
     private Vector3 initialSpawn;
     private bool spawning = false;
-    public static bool inAnimation = false;
     private bool dead = false;
+    private bool finalDead = false;
+
+    private static readonly int INITIAL_LIVES = 1;
+    public static int lives = INITIAL_LIVES;
+    public static bool inAnimation = false;
 
     // Start is called before the first frame update
     void Start()
@@ -27,6 +38,14 @@ public class DeathController : MonoBehaviour, IDeathSubscriber
     void Update()
     {
         inAnimation = false;
+        if(this.finalDead){
+            inAnimation = true;
+            this.animationTimerCount += Time.deltaTime;
+            if(this.animationTimerCount >= this.finalDeadAnimationTime){
+                lives = INITIAL_LIVES;
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
         if (this.dead)
         {
             inAnimation = true;
@@ -59,8 +78,18 @@ public class DeathController : MonoBehaviour, IDeathSubscriber
     public void OnDeath()
     {
         this.player.Unpossess();
-        this.dead = true;
-        audioSource.Play();
+        this.audioSourceDeath.Play();
         this.animator.SetTrigger("dead");
+        if((--lives) <= 0){
+            this.audioSourceFinalDeath.Play();
+            this.finalDead = true;
+            if(this.UIDeathAnimator != null){
+                UIDeathAnimator.SetTrigger("died");
+            }
+
+        }
+        else{
+            this.dead = true;
+        }
     }
 }
